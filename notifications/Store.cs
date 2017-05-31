9cf6a117a72ed7d1109b12be7ac020168d803944
Angel Guzman <xaklx@hotmail.com>
2017-05-30 23:03:43 -0400

    Add store

---
+++ notifications/Store.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace notifications
+{
+	public class Store
+	{
+		public int count = 0;
+
+		public delegate void onUpdateCalback(int newValue);
+
+		private static Store uniqueInstance = null;
+		private List<onUpdateCalback> onUpdate = null;
+
+
+		private Store()
+		{
+			onUpdate = new List<onUpdateCalback>();
+		}
+
+		public void addListener(onUpdateCalback callback)
+		{
+			onUpdate.Add(callback);
+		}
+
+		public static Store getUniqueInstance()
+		{
+			if (uniqueInstance == null)
+				uniqueInstance = new Store();
+
+			return uniqueInstance;
+		}
+
+		public void increaseCounter(int step)
+		{
+			count += step;
+
+			foreach(var callback in onUpdate)
+				callback(count);
+		}
+	}
+}
