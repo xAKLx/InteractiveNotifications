da9c86d68c2967b2ba8240b53ed707e1965d246e
Angel Guzman <xaklx@hotmail.com>
2017-05-30 23:04:27 -0400

    Add receiver

---
+++ notifications/MyReceiver.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Android.Content;
+using Android.Widget;
+using notifications;
+
+public class MyReceiver: BroadcastReceiver
+{
+	public override void OnReceive(Context context, Intent intent)
+	{
+		Store.getUniqueInstance()
+		     .increaseCounter(2);
+	}
+}
